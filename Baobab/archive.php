da58074c8dca77d8ce3e47d102c5220febf24d45
Joaquin Martel <joaquinmartel@gmail.com>
2012-06-06 16:08:16 -0400

    no message

--- Baobab/archive.php
+++
@@ -1,7 +0,0 @@
-<?php
-	/* Template Name: Àarchivo categorias?*/
-
-get_header(); ?>
-
-<?php wp_list_cats('use_desc_for_title=1'); ?>
-<?php get_footer(); ?>
--- Baobab/searchform.php
+++
@@ -1,3 +0,0 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>">
-	<input type="text" name="s" id="search" placeholder="Buscar" value="<?php the_search_query(); ?>" />	
-</form>
