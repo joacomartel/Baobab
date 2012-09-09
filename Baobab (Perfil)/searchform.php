2d221a9fdcd8bb1f6784a46b4efb2ec13e3d52ab
Joaquin Martel <joaquinmartel@gmail.com>
2012-09-09 14:02:16 -0300

    no message

--- Baobab (Perfil)/searchform.php
+++
@@ -1,3 +0,0 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>">
-	<input type="text" name="s" id="search" placeholder="<?php _e("Buscar","Baobab");?>" value="<?php the_search_query(); ?>" />	
-</form>
--- Baobab/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-	<div id="pagewrap">
-		<h1><a href="<?php echo site_url(); ?>"><?php bloginfo('title');?></a></h1>
-		<p><?php bloginfo('description');?></p>
-		<?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
-	</div><!-- pagewrap -->
-</footer>
-</body>
-</html>
